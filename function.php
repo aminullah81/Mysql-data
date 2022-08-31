5283beadc73ecd9f9e012bee4defb1e287fae0c1
aminullah81 <amin.awc@gmail.com>
2022-08-31 21:02:21 +0600

    function

---
+++ function.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+$a = 5;
+function sum() {
+    //$a = 6;
+    global $a;
+    var_dump($a);
+    echo $a;
+
+}
+
+sum();
+
+//echo $a;
+
+
+
+function sum($a, $b) {
+    var_dump($a);
+    var_dump($b);
+    return ($a + $b);
+
+}
+echo sum(7,8);
+
+
+function add(int $a , int $b) {
+    return $a + $b;
+}
+echo add(5, "8 days");
+
+
+using user define function write a php program to 
+check even or odd number ?
+*/
+
+$a = 58;
+if($a % 2 == 0){
+echo "this is even number";
+}else{
+echo "this is odd number";
+}
