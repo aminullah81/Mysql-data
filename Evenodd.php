85d1dc31476f0677a689ad817750c47f1ef7a471
aminullah81 <amin.awc@gmail.com>
2022-09-01 20:33:20 +0600

    Evenodd number using function

---
+++ Evenodd.php
@@ -0,0 +1,14 @@
+<?php
+
+for($i=1;$i<=100;$i++){
+    if($i%2===1){
+        echo $i;
+        echo",";
+    }
+}
+?>
+
+
+//using loop`
+//when $i%2==0 then output will be even number 
+//but when give $i%2==2 then output will be odd number.
